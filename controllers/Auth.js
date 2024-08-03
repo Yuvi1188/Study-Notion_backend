0343c81b9745823c886e0a9e02f55b2d7d6c3efb
Yuvi1188 <166728559+Yuvi1188@users.noreply.github.com>
2024-08-04 00:09:38 +0530

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -56,21 +56,21 @@ exports.signup = async (req, res) => {
     }
 
     // Find the most recent OTP for the email
-    const response = await OTP.find({ email }).sort({ createdAt: -1 }).limit(1)
-    console.log(response)
-    if (response.length === 0) {
-      // OTP not found for the email
-      return res.status(400).json({
-        success: false,
-        message: "The OTP is not valid",
-      })
-    } else if (otp !== response[0].otp) {
-      // Invalid OTP
-      return res.status(400).json({
-        success: false,
-        message: "The OTP is not valid",
-      })
-    }
+    // const response = await OTP.find({ email }).sort({ createdAt: -1 }).limit(1)
+    // console.log(response)
+    // if (response.length === 0) {
+    //   // OTP not found for the email
+    //   return res.status(400).json({
+    //     success: false,
+    //     message: "The OTP is not valid",
+    //   })
+    // } else if (otp !== response[0].otp) {
+    //   // Invalid OTP
+    //   return res.status(400).json({
+    //     success: false,
+    //     message: "The OTP is not valid",
+    //   })
+    // }
 
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10)
